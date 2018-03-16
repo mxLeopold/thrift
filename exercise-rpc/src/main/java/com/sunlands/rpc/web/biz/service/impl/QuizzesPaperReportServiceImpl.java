801e0da7ba24c26f155d36b3406c16c530e734c3
Qiuxia <chenqiuxia@sunlands.com>
2018-03-16 18:38:29 +0800

    随堂考统计数据接口 - 测试

--- exercise-rpc/src/main/java/com/sunlands/rpc/web/biz/service/impl/QuizzesPaperReportServiceImpl.java
+++ exercise-rpc/src/main/java/com/sunlands/rpc/web/biz/service/impl/QuizzesPaperReportServiceImpl.java
@@ -1,13 +1,27 @@
 package com.sunlands.rpc.web.biz.service.impl;
 
+import com.sunlands.rpc.web.biz.dao.QuizzesPaperReportMapper;
+import com.sunlands.rpc.web.biz.model.PaperDTO;
 import com.sunlands.rpc.web.biz.model.QuizzesPaperReportDTO;
 import com.sunlands.rpc.web.biz.service.QuizzesPaperReportService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.util.Assert;
 
+import java.util.Arrays;
 import java.util.List;
 
 public class QuizzesPaperReportServiceImpl implements QuizzesPaperReportService{
+    @Autowired
+    private QuizzesPaperReportMapper quizzesPaperReportMapper;
+
     @Override
     public List<QuizzesPaperReportDTO> getQuizzesPaperReport(String paperCode, String unitIdStr) {
-        return  null;
+        PaperDTO paperDTO = quizzesPaperReportMapper.selectPapeByCode(paperCode);
+        Assert.notNull(paperDTO, "试卷不存在");
+        QuizzesPaperReportDTO quizzesPaperReportDTO = new QuizzesPaperReportDTO();
+        quizzesPaperReportDTO.setPaperName(paperDTO.getName());
+        quizzesPaperReportDTO.setQuestionNum(paperDTO.getQuestionAmount());
+        // TODO: 2018/3/16 查询学员答题统计信息
+        return Arrays.asList(quizzesPaperReportDTO);
     }
 }
--- exercise-rpc/src/main/java/com/sunlands/rpc/web/handler/ApiWebServiceHandler.java
+++ exercise-rpc/src/main/java/com/sunlands/rpc/web/handler/ApiWebServiceHandler.java
@@ -1,11 +1,19 @@
 package com.sunlands.rpc.web.handler;
 
+import com.sunlands.rpc.web.biz.model.QuizzesPaperReportDTO;
+import com.sunlands.rpc.web.biz.service.QuizzesPaperReportService;
 import com.sunlands.rpc.web.service.ApiWebService;
 import com.sunlands.rpc.web.service.QuizzesPaperReport;
 import org.apache.thrift.TException;
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+import org.springframework.util.Assert;
+import org.springframework.util.CollectionUtils;
+import org.springframework.util.StringUtils;
 
+import javax.jws.Oneway;
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -18,10 +26,23 @@ import java.util.List;
  */
 @Component
 public class ApiWebServiceHandler implements ApiWebService.Iface {
-
-
+    @Autowired
+    private QuizzesPaperReportService quizzesPaperReportService;
     @Override
     public List<QuizzesPaperReport> getQuizzesPaperReport(String paperId, String unitIdStr) throws TException {
-        return null;
+        if (paperId == null || StringUtils.isEmpty(paperId)) {
+            throw new TException("paperId不能为空");
+        }
+        if (unitIdStr == null || StringUtils.isEmpty(unitIdStr)) {
+            throw new TException("unitIdStr不能为空");
+        }
+        List<QuizzesPaperReportDTO> quizzesPaperReportDTOS = quizzesPaperReportService.getQuizzesPaperReport(paperId, unitIdStr);
+        if (!CollectionUtils.isEmpty(quizzesPaperReportDTOS)) {
+            List<QuizzesPaperReport> quizzesPaperReports = new ArrayList<QuizzesPaperReport>();
+            BeanUtils.copyProperties(quizzesPaperReportDTOS, quizzesPaperReports);
+            return quizzesPaperReports;
+        } else {
+            return null;
+        }
     }
 }
