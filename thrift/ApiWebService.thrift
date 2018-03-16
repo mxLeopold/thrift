b6e99f8c770a4f8c003ea3c1ec14937525cf4c14
Qiuxia <chenqiuxia@sunlands.com>
2018-03-16 17:00:39 +0800

    随堂考统计数据接口

--- thrift/ApiWebService.thrift
+++ thrift/ApiWebService.thrift
@@ -1,16 +1,29 @@
 namespace java com.sunlands.rpc.web.service
 
-// 做题记录
-struct UserRecord {
-	1: i32 id;
-	2: i32 stuId;
-	3: string name; 
-	4: string exerciseType;
-	5: i32 subjectId;
+// 随堂考统计数据
+struct QuizzesPaperReport {
+    1: i32 answerNumber;
+    2: double answerTime;
+    3: double answerTimeAve;
+    4: string answerTimeAveString;
+    5: string answerTimeString;
+    6: i32 correct;
+    7: i32 correctAve;
+    8: double correctPercent;
+    9: double evaluatedScore;
+    10: string paperId;
+    11: string paperName;
+    12: double paperTotal;
+    13: double paperTotalAve;
+    14: double passRate;
+    15: i32 questionNum;
+    16: string subjectId;
+    17: i32 userPaperId;
+    18: i32 wrong;
 }
 
-service ApiCourseService {
-	// 学期
-	list<UserRecord> getUserRecord(1: i32 id, 2: i32 stuId);
+service ApiWebService {
+	// 查询随堂考列表
+	list<QuizzesPaperReport> getQuizzesPaperReport(1: string paperId, 2: string unitIdStr);
 
 }
