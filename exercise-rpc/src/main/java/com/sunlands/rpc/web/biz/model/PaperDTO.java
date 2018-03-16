3a5ad712e08be1571b121eef23e7e4ffd4fd6adc
Qiuxia <chenqiuxia@sunlands.com>
2018-03-16 17:46:03 +0800

    随堂考统计数据接口

---
+++ exercise-rpc/src/main/java/com/sunlands/rpc/web/biz/model/PaperDTO.java
@@ -0,0 +1,116 @@
+package com.sunlands.rpc.web.biz.model;
+
+import java.math.BigDecimal;
+import java.util.Date;
+
+public class PaperDTO {
+    private Integer id;
+    private String code;
+    private String name;
+    private String type;
+    private Integer questionAmount;
+    private BigDecimal avgDifficultyValue;
+    private BigDecimal totalScore;
+    private Date createTime;
+    private Date updateTime;
+    private String creator;
+    private String operator;
+    private Integer deleteFlag;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public Integer getQuestionAmount() {
+        return questionAmount;
+    }
+
+    public void setQuestionAmount(Integer questionAmount) {
+        this.questionAmount = questionAmount;
+    }
+
+    public BigDecimal getAvgDifficultyValue() {
+        return avgDifficultyValue;
+    }
+
+    public void setAvgDifficultyValue(BigDecimal avgDifficultyValue) {
+        this.avgDifficultyValue = avgDifficultyValue;
+    }
+
+    public BigDecimal getTotalScore() {
+        return totalScore;
+    }
+
+    public void setTotalScore(BigDecimal totalScore) {
+        this.totalScore = totalScore;
+    }
+
+    public Date getCreateTime() {
+        return createTime;
+    }
+
+    public void setCreateTime(Date createTime) {
+        this.createTime = createTime;
+    }
+
+    public Date getUpdateTime() {
+        return updateTime;
+    }
+
+    public void setUpdateTime(Date updateTime) {
+        this.updateTime = updateTime;
+    }
+
+    public String getCreator() {
+        return creator;
+    }
+
+    public void setCreator(String creator) {
+        this.creator = creator;
+    }
+
+    public String getOperator() {
+        return operator;
+    }
+
+    public void setOperator(String operator) {
+        this.operator = operator;
+    }
+
+    public Integer getDeleteFlag() {
+        return deleteFlag;
+    }
+
+    public void setDeleteFlag(Integer deleteFlag) {
+        this.deleteFlag = deleteFlag;
+    }
+
+}
