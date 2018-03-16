d047a76bc17be46fe6e8ba8cf24339715b77af76
yinhang <yinhang89@sunlands.com>
2018-03-16 13:03:27 +0800

    b端单元测试

---
+++ exercise-rpc/src/test/java/com/sunlands/rpc/test/WebServiceTest.java
@@ -0,0 +1,50 @@
+package com.sunlands.rpc.test;
+
+import com.sunlands.rpc.web.service.ApiWebService;
+import org.apache.thrift.protocol.TProtocol;
+import org.apache.thrift.protocol.TProtocolFactory;
+import org.apache.thrift.transport.THttpClient;
+import org.apache.thrift.transport.TTransport;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.context.embedded.LocalServerPort;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.context.junit4.SpringRunner;
+
+/**
+ * <p>Title:</p>
+ * <p>Description:</p>
+ *
+ * @author <a href="mailto:yinhang89@sunlands.com">尹行</a>
+ * @date 2018年03月16日
+ * @since v1.0
+ */
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@DirtiesContext
+public class WebServiceTest {
+
+    @Autowired
+    protected TProtocolFactory protocolFactory;
+
+    @LocalServerPort
+    protected int port;
+
+    protected ApiWebService.Client client;
+
+    @Before
+    public void setUp() throws Exception {
+        TTransport transport = new THttpClient("http://localhost:" + port + "/web");
+        TProtocol protocol = protocolFactory.getProtocol(transport);
+        client = new ApiWebService.Client(protocol);
+    }
+    @Test
+    public void testGroupMember() throws Exception {
+
+//        List<Member> members = client.getGroupMemberListByPerson(0, "wangzhuzhu");
+//        Assert.assertEquals(2,members.size());
+    }
+}
